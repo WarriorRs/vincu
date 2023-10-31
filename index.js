27c13790160f7ae1527128796a86f50b28dd0f79
Zerojav <javiermero8@gmail.com>
2023-10-30 22:23:23 -0500

    todo un reset

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { Pool } = require('pg');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 const port = 3000;
@@ -14,13 +15,46 @@ const pool = new Pool({
   port: 5432,
 });
 
+app.use(cookieParser()); // Configura el middleware cookieParser
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Configurar Express para servir archivos estáticos desde un directorio llamado "public"
+app.use(express.static(__dirname + '/public'));
+
+// Obtener index.html
 app.get('/', (req, res) => {
-  // Renderiza tu formulario HTML de inicio de sesión
+  res.sendFile(__dirname + '/index.html');
+});
+
+// Obtener login.html
+app.get('/login', (req, res) => {
   res.sendFile(__dirname + '/login.html');
 });
 
+// Definir una lista de elementos
+const elementos = [
+  'cerveza',
+  'whisky',
+  'aguardiente',
+  'ron',
+  'vodka',
+  'licor_hierbas',
+  'vinos_y_cocteles',
+  'tequila',
+  'aguas',
+  'gaseosas',
+  'snack'
+];
+
+// Iterar a través de los elementos y crear rutas
+elementos.forEach(elemento => {
+  app.get(`/${elemento}`, (req, res) => {
+    res.sendFile(__dirname + `/pages/${elemento}/${elemento}.html`);
+  });
+});
+
+  
 app.post('/login', (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
@@ -32,12 +66,20 @@ app.post('/login', (req, res) => {
       res.send('Error en la autenticación');
     } else {
       if (dbRes.rows.length === 1) {
-        res.send('Inicio de sesión exitoso');
+        // Autenticación exitosa, establece una cookie "logged_in" con valor "true"
+        res.cookie('logged_in', 'true');
+        res.redirect('/');
       } else {
-        res.send('Credenciales incorrectas');
+        res.redirect('/login?error=Credenciales%20incorrectas');
       }
     }
   });
+});
+
+// Agrega la ruta /logout
+app.get('/logout', (req, res) => {
+  res.clearCookie('logged_in'); // Elimina la cookie "logged_in"
+  res.redirect('/');
 });
 
 app.listen(port, () => {
